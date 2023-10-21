f621f948828816f2746d83829c144f3beb8ac094
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2023-10-21 13:23:15 +0700

    Baud rate can be passed as program argument

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,25 @@
 
 void usage() {
     printf("SmartRoadRadar-CLI\n\n");
-    printf("Run with COM-port name as argument.\n");
-    printf("Example: smart_road_radar.exe COM1\n");
+    printf("Run with COM-port name and baud rate as arguments.\n");
+    printf("Example: smart_road_radar.exe COM1 230400\n");
 }
 
 int main(int argc, char* argv[]) {
 
-    if (argc < 2 || argc > 2) {
+    if (argc < 3 || argc > 3) {
         usage();
         exit(-1);
     }
 
-    SmartRoadRadarCLI radar_cli((LPTSTR) argv[1]);
+    port_config config{};
+
+    config.baud_rate = atoi(argv[2]);
+    config.byte_size = BYTE_SIZE;
+    config.stop_bits = ONESTOPBIT;
+    config.parity = NOPARITY;
+
+    SmartRoadRadarCLI radar_cli((LPTSTR) argv[1], config);
     radar_cli.main_loop();
 
     exit(0);
